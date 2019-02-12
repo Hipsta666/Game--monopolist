adf8dbfa04f4f36f10e27af561b47bb49b6b3c5c
Pinkaso <pinsoo@bk.ru>
2019-02-12 16:03:11 +0300

    test version 1

--- main.py
+++ main.py
@@ -8,12 +8,23 @@ def advert(num, cost, form):
 
 
 def presentation(form):
-    print(form["клиенты"], round(form["капитал"]), form["производство"], form["счёт в банке"], form["место на рынке"],
-          form["цена товара"], form["год"])
+    print('|{:^6}|{:^12}|{:^16}|{:^16}|{:^10}|{:^10}|'.format('Год','Количество','Капитал','Счет','Место','Цена'))
+    print('|{:^6}|{:^12}|{:^16}|{:^16}|{:^10}|{:^10}|'.format('','клиентов','','в банке','на рынке','товара'))
+    print('='*77)
+    print('|{:^6}|{:^12}|{:^16}|{:^16}|{:^10}|{:^10}|'.format(form['год'],form["клиенты"],
+                                                        round(form["капитал"]),form["счёт в банке"],
+                                                        form["место на рынке"],form["цена товара"],
+                                                        form["год"]))
+    print('-' * 77)
+
+def score(money):
+    percent = random.choice([item for item in range(110,120)]) / 100
+    money *= percent
+    return money
 
 
-table = {"клиенты": 0, "капитал": 10000, "производство": 0, "счёт в банке": 700,
-         "место на рынке": 150, "цена товара": 50, "год": 0}
+table = {'год': 0, "клиенты": 375, "капитал": 32500, "счёт в банке": 15000,
+         "место на рынке": 148, "цена товара": 50}
 presentation(table)
 cost_advert = table["цена товара"] * 0.6
 
@@ -28,6 +39,6 @@ for year in range(1000):
     advert(qs_1, cost_advert, table)
     presentation(table)
     qs_2 = int(input(message_2))
-
+    score()
 
 
